a89e3e295e55883a56856ba822c39a0098c52f5f
samira hamada <samirahamadaabdelaleem123@gmail.com>
2026-09-16 09:52:27 +0300

    Add project files.

---
+++ Assignment03CSharpBasics/Program.cs
@@ -0,0 +1,21 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Assignment03CSharpBasics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 1st Question
+            // Create a one - dimensional array double[] prices with the values 25.5, 40.0, 33.75.Print
+            //the second price(index 1). 
+            double[] prices = { 25.5, 40.0, 33.75 };
+            Console.WriteLine(prices[1]);
+            #endregion
+
+         
+
+            
+        }
+    }
+}
