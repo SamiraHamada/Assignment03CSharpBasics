97854ad73b266d21c4077b77f6b9acb64b016f1a
samira hamada <samirahamadaabdelaleem123@gmail.com>
2026-09-16 10:01:34 +0300

    #region 2nd Question     // Create a 2x2 multidimensional array int[,] shelfCopies where shelf 0 has 3, 5 copies and     // shelf 1 has 1, 4 copies.Print the number of copies on shelf 1, slot 0.     int[,] shelfCopies = { { 3, 5 }, { 1, 4 } };     Console.WriteLine(shelfCopies[1, 0]);     #endregion

--- Assignment03CSharpBasics/Program.cs
+++ Assignment03CSharpBasics/Program.cs
@@ -13,10 +13,16 @@ namespace Assignment03CSharpBasics
             Console.WriteLine(prices[1]);
             #endregion
 
+            #region 2nd Question
+            // Create a 2x2 multidimensional array int[,] shelfCopies where shelf 0 has 3, 5 copies and
+            // shelf 1 has 1, 4 copies.Print the number of copies on shelf 1, slot 0.
+            int[,] shelfCopies = { { 3, 5 }, { 1, 4 } };
+            Console.WriteLine(shelfCopies[1, 0]);
+            #endregion
 
-         
 
-            
+
+
         }
     }
 }
